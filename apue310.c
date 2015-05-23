b65ef9e38faa0871f4f4bc780ac2532d5ae8d4bf
刘常德 <chandler@liuchangdedeMacBook-Pro.local>
2015-05-23 20:27:56 +0800

    add apue310.c

---
+++ apue310.c
@@ -0,0 +1,38 @@
+//创建线程和打印线程ID
+//2015-05-23
+//by chandler
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+pthread_t ntid;
+
+void printids(const char *s)
+{
+	pid_t pid;
+	pthread_t tid;
+	
+	pid = getpid();
+	tid = pthread_self();
+	printf("%s pid %lu tid %lu (0x%lx)\n",s,(unsigned long)pid,(unsigned long)tid,(unsigned long)tid);
+}
+
+void *thr_fn(void *arg)
+{
+	printids("new thread: ");
+	return ((void *)0);
+}
+
+int main(void)
+{
+	int err;
+	
+	err = pthread_create(&ntid,NULL,thr_fn,NULL);
+	if (err != 0)
+		printf("can't create thread\n");
+	printids("main thread:");
+	sleep(1);
+	exit(0);
+}
